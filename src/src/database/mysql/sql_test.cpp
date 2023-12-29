62c0b87663b39aa14fa10fcb71662b89f26d35d9
wlk <15938712580@163.com>
2023-12-29 23:14:59 +0800

    Singleton

--- src/src/database/mysql/sql_test.cpp
+++ src/src/database/mysql/sql_test.cpp
@@ -5,13 +5,23 @@ int main() {
   MYSQL mysql;
   mysql_init(&mysql);
   mysql_options(&mysql, MYSQL_SET_CHARSET_NAME, "utf8");
-  if (!mysql_real_connect(&mysql, "192.168.2.111", "root", "erjk", "henan", 3306, NULL, 0)) {
+  if (!mysql_real_connect(&mysql, "192.168.2.112", "root", "erjk", "henan", 3306, NULL, 0)) {
     std::cout <<"sql_errno="<<mysql_errno(&mysql)<<std::endl;
     return -1;
   }
   std::cout << "sql connect successed!" << std::endl;
 
   std::string query_str="select * from zhoukou";
+
+  std::string insert_str="insert into zhoukou (Date,District_County,Town,Community,Amount,Price) values('2021-12-25','郸城','南区','蔡庄',200,3);";
+
+  if(mysql_query(&mysql,insert_str.c_str())){
+    printf("error:%s\n", mysql_error(&mysql));
+    perror("insert error");
+  }
+
+
+
   if(mysql_query(&mysql,query_str.c_str())){
     std::cout<<"查询失败"<<std::endl;
     return -1;
