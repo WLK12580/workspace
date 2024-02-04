c7083312222e1359406aa9e3b9c71d471bc85518
wlk <15938712580@163.com>
2024-02-04 17:05:44 +0800

    output

---
+++ CMakeCommon.cmake
@@ -0,0 +1,5 @@
+
+
+set(BoostUse ON)
+find_package(Boost COMPONENTS ALL)
+include_directories(${Boost_INCLUDE_DIR})
--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 builddir="./build"
 if [ -d ${builddir} ];then
-    echo "delete build"
+    echo "-- delete build"
     rm -rf ${builddir}
 fi  
+echo "-- delete build directory done!"
 cmake -B build
 cmake --build build/
