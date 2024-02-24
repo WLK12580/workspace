b92a7cd29aae9b36d88a01e24c464c11a69e4206
wlk <15938712580@163.com>
2024-02-24 18:46:00 +0800

    img

--- src/imageprocess/opencvtest.cpp
+++ src/imageprocess/opencvtest.cpp
@@ -2,7 +2,11 @@
 #include<iostream>
 
 int main() {
-  cv::Mat img = cv::imread("/home/wlk/workspace/autocar/Image/image.png", cv::IMREAD_COLOR);
+  cv::Mat img = cv::imread("./Image/test.jpg", cv::IMREAD_COLOR); //路径是基于编译时的cmake路径的，而不是相对于当前原文将的路径
+  if(img.empty()){
+    std::cout<<"image read failed\n";
+  }
+  cv::namedWindow("img",cv::WINDOW_AUTOSIZE);
   cv::imshow("img", img);
   cv::waitKey(0);
   return 0;
