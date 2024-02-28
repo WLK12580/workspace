4a633102c605d36566d2a99bc785bd878cba87a0
wlk <15938712580@163.com>
2024-02-28 22:22:04 +0800

    modify

--- internal_deps/framework/MySql/mysqlMain.cpp
+++ internal_deps/framework/MySql/mysqlMain.cpp
@@ -9,7 +9,7 @@ int main(){
     map.emplace("username","xiaosun");
     map.emplace("passwd","986532");
     map.emplace("birthdate","1993-08-21");
-    std::cout<<"insertStatus="<<mysql.insertToTable(tableName,map)<<"\n";
+    std::cout<<"insertSuccessed="<<mysql.insertToTable(tableName,map)<<"\n";
 
     return 0;
 }
